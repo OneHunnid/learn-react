196556299a8223f05dc2fa762e0f5dff71d2751f
Chris <climbrick1989@gmail.com>
2017-03-02 15:14:10 -0500

    Adding bootstrap

--- 00Base-Template/src/client/scripts/components/Header.js
+++ 00Base-Template/src/client/scripts/components/Header.js
@@ -9,6 +9,7 @@ class Header extends React.Component {
     return (
       <header>
         <h1><Link to="/">Durian</Link></h1>
+        <button type="button" className="btn btn-primary">Primary</button>
       </header>
     )
   }
--- 00Base-Template/webpack.config.js
+++ 00Base-Template/webpack.config.js
@@ -59,6 +59,10 @@ module.exports = {
         }
       },
       {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      },
+      {
         test: /\.scss$/,
         loader: ExtractTextPlugin.extract('style', ['css', 'postcss', 'sass'])
       }
