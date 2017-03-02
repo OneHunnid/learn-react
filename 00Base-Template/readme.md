a324e5c4e1bcfbbc9c9794ad1cc03a1885a2306e
Chris <climbrick1989@gmail.com>
2017-03-02 16:36:32 -0500

    Adding proptypes and updating readme.md

--- 00Base-Template/readme.md
+++ 00Base-Template/readme.md
@@ -1,7 +1,3 @@
-# Durian
+# Base Template
 
-React, Redux, and Sass Webpack Starter Kit
-
-1) Clone repo
-2) run `npm install`
-3) Open localhost:8080 and enjoy!
+A React, Redux, Sass, Bootstrap and Webpack starter kit that includes deployment to Heroku.
--- 00Base-Template/src/client/scripts/routes/Home.js
+++ 00Base-Template/src/client/scripts/routes/Home.js
@@ -17,12 +17,24 @@ class Home extends React.Component {
     }
     else {
       return (
-        <div>{this.props.message}!</div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h2>{this.props.message}</h2>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet nulla tortor. Duis id rutrum dolor. Fusce eu quam viverra, tristique justo quis, rhoncus augue. In id risus euismod, finibus dui eget, lacinia lorem. Curabitur euismod quam et lectus fermentum, vel maximus libero tincidunt. Nunc ac dignissim nisl. Nullam lobortis tellus vitae dolor vulputate lobortis. Morbi porttitor odio et ante porttitor, vel rhoncus mauris pulvinar.</div>
+          </div>
+        </div>
       )
     }
   }
 }
 
+Home.propTypes = {
+  message: React.PropTypes.string
+}
 
 const mapStateToProps = (store) => {
   return {
