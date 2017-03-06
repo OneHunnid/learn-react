a8fffdf2a7baeb8d82dbca1bb22a6c1f9ca6927e
Chris <climbrick1989@gmail.com>
2017-03-06 13:29:46 -0500

    Adding react-bootstrap and integrating it with examples.

--- 00Base-Template/src/client/scripts/components/Header.js
+++ 00Base-Template/src/client/scripts/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router'
+import { Nav, NavItem, Input, Navbar, Button } from 'react-bootstrap'
 
 class Header extends React.Component {
   constructor(props) {
@@ -7,59 +8,23 @@ class Header extends React.Component {
   }
   render() {
     return (
-      <header>
-        <nav class="navbar navbar-default">
-          <div class="container-fluid">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#">Base Template</a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">Link</a></li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">One more separated link</a></li>
-                  </ul>
-                </li>
-              </ul>
-              <form class="navbar-form navbar-left">
-                <div class="form-group">
-                  <input type="text" class="form-control" placeholder="Search" />
-                </div>
-                <button type="submit" class="btn btn-default">Submit</button>
-              </form>
-              <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Link</a></li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </header>
+        <Navbar className="navbar--no-margin-bottom">
+         <Navbar.Header>
+           <Navbar.Brand>
+             <a href="#">Base Template</a>
+           </Navbar.Brand>
+           <Navbar.Toggle />
+         </Navbar.Header>
+         <Navbar.Collapse>
+           <Nav>
+             <NavItem eventKey={1} href="#">Link 1</NavItem>
+             <NavItem eventKey={2} href="#">Link 2</NavItem>
+           </Nav>
+           <Navbar.Form pullRight>
+             <Button type="submit">Submit</Button>
+           </Navbar.Form>
+         </Navbar.Collapse>
+        </Navbar>
     )
   }
 }
--- 00Base-Template/src/client/scripts/routes/Home.js
+++ 00Base-Template/src/client/scripts/routes/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { fetchMessage } from './../actions/fetchMessage'
+import { Grid, Row, Col, Button, Jumbotron } from 'react-bootstrap'
 
 class Home extends React.Component {
   constructor(props) {
@@ -17,15 +18,21 @@ class Home extends React.Component {
     }
     else {
       return (
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <h2>{this.props.message}</h2>
+        <div className="home">
+          <Jumbotron>
+            <div className="container text-center">
+              <h1>Base template</h1>
+              <p>{this.props.message}</p>
+              <Button>Learn More</Button>
             </div>
-          </div>
-          <div className="row">
-            <div className="col-md-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet nulla tortor. Duis id rutrum dolor. Fusce eu quam viverra, tristique justo quis, rhoncus augue. In id risus euismod, finibus dui eget, lacinia lorem. Curabitur euismod quam et lectus fermentum, vel maximus libero tincidunt. Nunc ac dignissim nisl. Nullam lobortis tellus vitae dolor vulputate lobortis. Morbi porttitor odio et ante porttitor, vel rhoncus mauris pulvinar.</div>
-          </div>
+          </Jumbotron>
+          <Grid>
+            <Row>
+              <Col xs={12} md={4}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet nulla tortor. Duis id rutrum dolor. Fusce eu quam viverra, tristique justo quis, rhoncus augue. In id risus euismod, finibus dui eget, lacinia lorem. Curabitur euismod quam et lectus fermentum, vel maximus libero tincidunt. Nunc ac dignissim nisl. Nullam lobortis tellus vitae dolor vulputate lobortis. Morbi porttitor odio et ante porttitor, vel rhoncus mauris pulvinar.</Col>
+              <Col xs={12} md={4}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet nulla tortor. Duis id rutrum dolor. Fusce eu quam viverra, tristique justo quis, rhoncus augue. In id risus euismod, finibus dui eget, lacinia lorem. Curabitur euismod quam et lectus fermentum, vel maximus libero tincidunt. Nunc ac dignissim nisl. Nullam lobortis tellus vitae dolor vulputate lobortis. Morbi porttitor odio et ante porttitor, vel rhoncus mauris pulvinar.</Col>
+              <Col xs={12} md={4}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet nulla tortor. Duis id rutrum dolor. Fusce eu quam viverra, tristique justo quis, rhoncus augue. In id risus euismod, finibus dui eget, lacinia lorem. Curabitur euismod quam et lectus fermentum, vel maximus libero tincidunt. Nunc ac dignissim nisl. Nullam lobortis tellus vitae dolor vulputate lobortis. Morbi porttitor odio et ante porttitor, vel rhoncus mauris pulvinar.</Col>
+            </Row>
+          </Grid>
         </div>
       )
     }
